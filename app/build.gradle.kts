b014bdd8b2f910974bbda25e967206e2598d33d2
HotarunIchijou <hotarunichijou@ik.me>
2023-09-23 12:18:34 +0300

    Remove unneeded signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
