93cb1d4225ad46940cbfda4ee67da802fa97ede2
HotarunIchijou <hotarunichijou@ik.me>
2023-11-23 12:55:16 +0300

    build: bump com.google.material to 1.12.0-alpha01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0-beta01")
+    implementation("com.google.android.material:material:1.12.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 }
