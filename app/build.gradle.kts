7c3a4b37bca99cbb7ef66c2c9722a3c409da577c
Kaorun <hotarunichijou@ik.me>
2024-08-29 00:35:00 +0300

    build: bump androidx.core to 1.13.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
